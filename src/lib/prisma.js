cf7ab859237f9653bf4c748850884c542f2c3c60
Kenz8489 <stark858981@gmail.com>
2025-08-30 15:07:55 +0530

    prisma client import error fix#2

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,5 +1,4 @@
-// import { PrismaClient } from '../generated/prisma'
-import { PrismaClient } from '../generated/prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 
 const globalForPrisma = globalThis
