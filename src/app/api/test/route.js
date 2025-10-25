3263b9571002e8c4621f69a65634e7784c189bc4
Kenz8489 <stark858981@gmail.com>
2025-10-25 21:42:38 +0530

    test api updated

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -2,17 +2,7 @@ import { getPlaceBySlug, insertPlace } from "../../../lib/Places";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    const data = await getPlaceBySlug('shinjuku-tokyo-japan')
+    const data = await getPlaceBySlug('morocco')
     return NextResponse.json(data)
 }
-
-export async function POST() {
-    const data = await insertPlace()
-    return new NextResponse(JSON.stringify(data), {
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    })
-}
-
 
